8f5cbc45f5e5273375ef715d169eff1ae32ed4bb
Moses Sichei <sichei@yahoo.co.uk>
2017-09-16 19:30:59 +0000

    message

---
+++ ExData_Plotting1/Plot3.R
@@ -0,0 +1,31 @@
+#Code to download,read data and plot Plot3.png
+#download and unzip file
+temp <- tempfile()
+
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+
+unzip (temp,"household_power_consumption.txt")
+
+unlink(temp)
+
+
+#read data already downloaded to a local computer
+data_full <- read.table("household_power_consumption.txt", header=TRUE, na.strings="?", sep=";")
+data <- data_full[(data_full$Date=="1/2/2007" | data_full$Date=="2/2/2007" ), ]
+
+#format date and time
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+dateTime <- paste(data$Date, data$Time)
+data$DateTime <- as.POSIXct(dateTime)
+
+#plot3
+with(data, {
+  plot(Sub_metering_1~DateTime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="", cex=0.8)
+  lines(Sub_metering_2~DateTime, col='Red')
+  lines(Sub_metering_3~DateTime, col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=1, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex=0.8)
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
